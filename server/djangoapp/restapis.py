a18756bec3722a7ff648e29524b9da2d845d6eaf
MatthewRuizDiaz <matthew.ruizdiaz@gmail.com>
2025-04-21 20:41:23 -0400

    added rest apis: get request, post review, sentiment analyzer and created more views to utilize them: get dealer details, add review, get dealer reviews

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,5 +1,5 @@
 # Uncomment the imports below before you add the function code
-# import requests
+import requests
 import os
 from dotenv import load_dotenv
 
@@ -13,10 +13,31 @@ sentiment_analyzer_url = os.getenv(
 
 # def get_request(endpoint, **kwargs):
 # Add code for get requests to back end
-
+def get_request(endpoint, **kwargs):
+    request_url = f"{backend_url}{endpoint}"
+    try:
+        response = requests.get(request_url, params=kwargs)
+        response.raise_for_status()
+        return response.json()
+    except RequestException as e:
+        print("Network exception ocurred")
 # def analyze_review_sentiments(text):
 # request_url = sentiment_analyzer_url+"analyze/"+text
 # Add code for retrieving sentiments
+def analyze_review_sentiments(text):
+    request_url = sentiment_analyzer_url+'analyze/'+text
+    try:
+        response = requests.get(request_url)
+        response.raise_for_status()
+        return response.json()
+    except:
+        print("unexpected error")
 
-# def post_review(data_dict):
-# Add code for posting review
+def post_review(data_dict):
+    request_url = backend_url+"/insert_review"
+    try:
+        response = requests.post(request_url,json=data_dict)
+        print(response.json())
+        return response.json()
+    except:
+        print("Network exception occurred")
