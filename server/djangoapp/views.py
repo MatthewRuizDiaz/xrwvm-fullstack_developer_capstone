53fd3ac39f6e93c3660a95b46fc9e49a7fb719c3
MatthewRuizDiaz <matthew.ruizdiaz@gmail.com>
2025-04-29 22:12:35 -0400

    fixed views.py again

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -135,7 +135,7 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            post_review(data)
+            response = post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({
